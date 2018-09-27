4f1ee1235bc690fdcfa53cc726ffd11ced371a2a
David Panjaitan <david@atomykorea.id>
2018-09-27 16:24:44 +0700

    Adding crud function

---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,45 @@
+@extends('products.layout')
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Edit Product</h2>
+        </div>
+        <div class="pull-right">
+           <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<form action="{{ route('products.update',$product->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+   
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Name: {{ $product->name }}</strong>                    
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Detail: {{ $product->detail }}</strong>                    
+                </div>
+            </div>          
+        </div>
+
+    </form>
+
+@endsection
